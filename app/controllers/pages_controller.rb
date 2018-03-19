1bf9ee729a397696761cfde283628bf030e4dcb3
Yuma Soga <ysoga19@gmail.com>
2018-03-19 08:37:14 +0000

    Implement Pages by MD

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def index
+    @title = 'ページ一覧'
+    @docs  = Document.all
+    @url   = request.url
+  end
+
+  def show
+    @doc = Document.new(params[:id])
+    redirect_to root_url unless @doc.exists?
+    @content = Kramdown::Document.new(@doc.content).to_html
+    @url     = request.url
+  end
+end
---
+++ app/models/document.rb
@@ -0,0 +1,38 @@
+class Document
+  attr_reader :id, :filename
+  DOCS_PATH = 'db/pages'
+  URL_PATH  = 'pages'
+
+  class << self
+    def all
+      Dir.glob("#{DOCS_PATH}/*.md").map do |filename|
+        Document.new(File.basename(filename, '.*'))
+      end
+    end
+  end
+
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def path
+    "#{DOCS_PATH}/#{self.filename}.md"
+  end
+
+  def url
+    "/#{URL_PATH}/#{self.filename}"
+  end
+
+  def exists?
+    return false if path.include? "\u0000"
+    File.exists?(path)
+  end
+
+  def title
+    @title ||= exists? ? self.content.lines.first[2..-1].strip.gsub('<br>', '') : ''
+  end
+
+  def content
+    @content ||= exists? ? File.read(path) : ''
+  end
+end
