d5f91a62e00427870a541a6fc73fcebacb307cb0
Yuma Soga <ysoga19@gmail.com>
2018-04-01 17:29:53 +0000

    Delete CodeCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,3 @@
-require 'simplecov'
-
-SimpleCov.start 'rails'
-
-if ENV['CODECOV_TOKEN']
-  require 'codecov'
-  SimpleCov.formatter =  SimpleCov::Formatter::Codecov
-end
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
