db6669d17415bde2584feed3bd85e32f1d7507ba
Yuma Soga <ysoga19@gmail.com>
2018-04-02 15:21:34 +0000

    Add test for full_title in ApplicationHelper

---
+++ spec/helper/application_helper_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  include(ApplicationHelper)
+  describe "full_title" do
+    it "plovided title is nil" do
+      plovided_title = nil
+      expect(full_title(plovided_title)).to eq "CoderDojo Konan Okayama Official Site"
+    end
+    it "plovided title is empty" do
+      plovided_title = ""
+      expect(full_title(plovided_title)).to eq "CoderDojo Konan Okayama Official Site"
+    end
+    it "has plovided title" do
+      plovided_title = "Test Page"
+      expect(full_title(plovided_title)).to eq "#{plovided_title} | CoderDojo Konan Okayama Official Site"
+    end
+  end
+end
