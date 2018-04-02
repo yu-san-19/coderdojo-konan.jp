863bc5f3f7083f593850b0b6ef76918647366f58
Yuma Soga <ysoga19@gmail.com>
2018-04-02 15:21:26 +0000

    Add test for full_title in ApplicationHelper

--- spec/controllers/staticpages_controller_spec.rb
+++ spec/controllers/staticpages_controller_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe StaticpagesController, type: :controller do
+describe StaticpagesController, type: :controller do
   describe "Request GET #index" do
     before do
       get :index
