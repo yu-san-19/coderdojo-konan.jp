512162e7973cf0a6f6f432e3308d20df73fb50e2
Yuma Soga <ysoga19@gmail.com>
2018-04-02 14:11:05 +0000

    Add RSpec Code for PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,16 @@ class PagesController < ApplicationController
 
   def index
     @title = 'ページ一覧'
-    @pages  = Page.all
-    @url   = request.url
+    @pages = Page.all
+    @url = request.url
   end
 
   def show
     @page = Page.new(params[:id])
+    unless @page.exists?
+      render :status => 404
+    end
     @content = Kramdown::Document.new(@page.content).to_html
-    @url     = request.url
+    @url = request.url
   end
 end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe PagesController, type: :controller do
+  describe "Request GET #index" do
+    it "can show @pages" do
+      get :index
+      expect(assigns(:pages).first.title).to eq Page.all.first.title
+    end
+  end
+  describe "Request GET #show" do
+    it "can show @content" do
+      param    = 'visit-interview'
+      get :show, :id => param
+      page      = Page.new(param)
+      expected = Kramdown::Document.new(page.content).to_html
+      expect(assigns(:content)).to eq expected
+    end
+    describe "when invalid filename" do
+      before do
+        get :show, :id => 'not_found_hoge'
+      end
+      it 'redirect to 404 page?' do
+        expect(response.status).to eq 404
+      end
+    end
+  end
+end
