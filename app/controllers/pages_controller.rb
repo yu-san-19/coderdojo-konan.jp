72418019610ed6cb615a8f055e8c33b026ee293a
Yuma Soga <ysoga19@gmail.com>
2018-04-02 10:50:16 +0000

    Change Model Name from Document to Page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,12 @@ class PagesController < ApplicationController
 
   def index
     @title = 'ページ一覧'
-    @docs  = Document.all
+    @docs  = Page.all
     @url   = request.url
   end
 
   def show
-    @doc = Document.new(params[:id])
+    @doc = Page.new(params[:id])
     redirect_to root_url unless @doc.exists?
     @content = Kramdown::Document.new(@doc.content).to_html
     @url     = request.url
