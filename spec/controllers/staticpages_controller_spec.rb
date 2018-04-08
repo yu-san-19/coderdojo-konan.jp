7e3ac8c4ea95d6e6d121f0d6fe5736985f29c689
Yuma Soga <ysoga19@gmail.com>
2018-04-08 09:35:00 +0000

    Rewrite Test for StaticPages Controller

--- spec/controllers/staticpages_controller_spec.rb
+++ spec/controllers/staticpages_controller_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe StaticpagesController, type: :controller do
   describe "Request GET #index" do
-    before do
-      get :index
+    describe "Get latest event" do
+      it "return correct date" do
+      end
     end
-    it "set @next_event_url" do
-      today = DateTime.now
-      result = JSON.parse(RestClient.get('https://connpass.com/api/v1/event/?series_id=3786&order=1&keyword=CoderDojo+%E5%B2%A1%E5%B1%B1+%E5%B2%A1%E5%8D%97').body)
-      next_event_url = result["events"][0]["event_url"]
-      next_event_date = result["events"][0]["started_at"]
-      if today < next_event_date then
-        expect(assigns[:next_event_url]).to eq next_event_url
-      else
-        expect(assigns[:next_event_url]).to eq next_event_is_still_planned_url
+    describe "@next_event_url" do
+      context "when next event is not still planned" do
+        it "return nil" do
+        end
+      end
+      context "when next event is already planned" do
+        it "return expected event url" do
+        end
       end
     end
   end
