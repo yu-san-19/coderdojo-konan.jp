55bb691598e47bc5c6f689c9e9afc0a244c4918b
Yuma Soga <ysoga19@gmail.com>
2018-04-18 13:51:46 +0000

    resolved #3 Add Hepler for Next Event URL

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,24 +1,8 @@
 class StaticpagesController < ApplicationController
   def index
-    latest_event = get_latest_event
-    @next_event_url = if Date.parse(latest_event['started_at']) > Date.today
-                        latest_event['event_url']
-                      else
-                        nil
-                      end
   end
   def next_event_is_still_planned
   end
   def ninjas_works
-  end
-
-  private
-  def get_latest_event
-    response = rest_client.get(params: {:series_id => 3786, :order => 2})
-    json = JSON.parse(response.body)
-    json['events'].first
-  end
-  def rest_client
-    RestClient::Resource.new 'https://connpass.com/api/v1/event/'
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,23 @@ module ApplicationHelper
         "#{page_title} | #{base_title}"
       end
   end
+
+  def next_event_url_helper
+    latest_event = get_latest_event
+    if Date.parse(latest_event['started_at']) > Date.today
+      latest_event['event_url']
+    else
+      nil
+    end
+  end
+
+  private
+  def get_latest_event
+    response = rest_client.get(params: {:series_id => 3786, :order => 2})
+    json = JSON.parse(response.body)
+    json['events'].first
+  end
+  def rest_client
+    RestClient::Resource.new 'https://connpass.com/api/v1/event/'
+  end
 end
