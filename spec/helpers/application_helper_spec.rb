f193dd6444a4742b223099c4f68faa5fb5f65486
Yuma Soga <ysoga19@gmail.com>
2018-04-19 13:34:44 +0000

    Add spec test application_helper_spec.rb

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe ApplicationHelper do
   describe "full_title" do
-    describe "when plovided title is nil or empty" do
+    context "when plovided title is nil or empty" do
       it "plovided title is nil" do
         plovided_title = nil
         expect(full_title(plovided_title)).to eq "CoderDojo Konan Okayama Official Site"
@@ -12,9 +12,15 @@ describe ApplicationHelper do
         expect(full_title(plovided_title)).to eq "CoderDojo Konan Okayama Official Site"
       end
     end
-    it "has plovided title" do
-      plovided_title = "Test Page"
-      expect(full_title(plovided_title)).to eq "#{plovided_title} | CoderDojo Konan Okayama Official Site"
+    context "when it is plovided then" do
+      it "has plovided title" do
+        plovided_title = "Test Page"
+        expect(full_title(plovided_title)).to eq "#{plovided_title} | CoderDojo Konan Okayama Official Site"
+      end
+    end
+  end
+  describe "Next event url helper" do
+    it "is return true url when next event is always planned" do
     end
   end
 end
