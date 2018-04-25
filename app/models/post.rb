b40ea8116619715c88541465641b6aa5449cf152
Yuma Soga <ysoga19@gmail.com>
2018-04-25 10:10:05 +0000

    #25 Add public and unpublished function
    公開・未公開のフィルター・スコープなどを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   extend FriendlyId
   friendly_id :slug, use: :slugged
   
+  scope :published, -> { where("published_at IS NOT NULL").where("published_at <= ?", Time.now) }
+  scope :draft, -> { where("published_at IS NULL OR published_at > ?", Time.now) }
+  
   def should_generate_new_friendly_id?
     slug_changed? || super
   end
