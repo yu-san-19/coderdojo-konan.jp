8b6869e1937aadd168bff72205eb1ffb02127e3f
Yuma Soga <ysoga19@gmail.com>
2018-04-25 14:30:51 +0000

    Time Zone Reset

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Workspace
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     
-    config.time_zone = 'Tokyo'
+    # config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
