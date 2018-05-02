7e31228f6fd29f8c291d42eba5ed17e2e8c24505
Yuma Soga <ysoga19@gmail.com>
2018-05-02 14:02:53 +0000

    Add spec

---
+++ spec/controllers/admin/sessions_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Admin::SessionsController, type: :controller do
+
+  # describe "GET #index" do
+  #   it "returns http success" do
+  #     get :index
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "GET #new" do
+  #   it "returns http success" do
+  #     get :new
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "GET #login" do
+  #   it "returns http success" do
+  #     get :login
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+end
